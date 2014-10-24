6f798ba1f75360e6c66260b0ac2de76efa0a7d1d
Marc-André Lureau <marcandre.lureau@gmail.com>
2014-10-24 16:59:30 +0200

    Split spice.h
    Make it easier to read the Spice server API.

--- server/spice-version.h
+++ server/spice-version.h
@@ -15,20 +15,13 @@
  *  License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef SPICE_H_
-#define SPICE_H_
+#ifndef SPICE_VERSION_H_
+#define SPICE_VERSION_H_
 
-#define SPICE_H_INSIDE
+#if !defined(SPICE_H_INSIDE) && !defined(SPICE_SERVER_INTERNAL)
+#error "Only spice.h can be included directly."
+#endif
 
-#include "spice-version.h"
-#include "spice-core.h"
-#include "spice-server.h"
-#include "spice-qxl.h"
-#include "spice-input.h"
-#include "spice-audio.h"
-#include "spice-char.h"
-#include "spice-migration.h"
+#define SPICE_SERVER_VERSION 0x000c06 /* release 0.12.6 */
 
-#undef SPICE_H_INSIDE
-
-#endif /* SPICE_H_ */
+#endif /* SPICE_VERSION_H_ */
