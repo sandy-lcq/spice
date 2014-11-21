1876971442ef808b5dcdaa5dc12df617f2179cb5
Christophe Fergeau <cfergeau@redhat.com>
2014-11-21 11:01:17 +0100

    client: Remove client code
    The client has been superseded by virt-viewer (
    http://virt-manager.org/download/sources/virt-viewer/ ) and is no longer
    being maintained.

--- client/windows/generate.bat
+++
@@ -1,2 +0,0 @@
-python ..\..\spice-common\spice_codegen.py --generate-demarshallers  --client --include messages.h ..\..\spice-common\spice.proto ..\generated_demarshallers.c
-python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include messages.h --include client_marshallers.h --client ..\..\spice-common\spice.proto ..\generated_marshallers.c
--- client/windows/generate1.bat
+++
@@ -1,2 +0,0 @@
-python ..\..\spice-common\spice_codegen.py --generate-demarshallers --client --include messages.h --prefix 1 --ptrsize 8 ..\..\spice-common\spice1.proto ..\generated_demarshallers1.c
-python ..\..\spice-common\spice_codegen.py --generate-marshallers -P --include messages.h  --include client_marshallers.h --client --prefix 1 --ptrsize 8 ..\..\spice-common\spice1.proto ..\generated_marshallers1.c
