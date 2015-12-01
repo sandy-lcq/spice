1ea55284d97b1c55e2b7637ef42635d162a181c5
Frediano Ziglio <fziglio@redhat.com>
2015-12-01 16:46:51 +0000

    memslot: change some spice_assert to spice_return_if_fail
    Signed-off-by: Marc-André Lureau <marcandre.lureau@gmail.com>
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
    Acked-by: Jonathon Jongsma <jjongsma@redhat.com>

--- server/red_memslots.c
+++ server/red_memslots.c
@@ -168,8 +168,8 @@ void memslot_info_add_slot(RedMemSlotInfo *info, uint32_t slot_group_id, uint32_
 
 void memslot_info_del_slot(RedMemSlotInfo *info, uint32_t slot_group_id, uint32_t slot_id)
 {
-    spice_assert(info->num_memslots_groups > slot_group_id);
-    spice_assert(info->num_memslots > slot_id);
+    spice_return_if_fail(info->num_memslots_groups > slot_group_id);
+    spice_return_if_fail(info->num_memslots > slot_id);
 
     info->mem_slots[slot_group_id][slot_id].virt_start_addr = 0;
     info->mem_slots[slot_group_id][slot_id].virt_end_addr = 0;
