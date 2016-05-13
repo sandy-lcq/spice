sandy-lcq/spice

commit 18d3876b78c7f19c577da000d322dbd44e63e456

UpgradeItem: use base PipeItem for refcounting
No need to re-implement refcounting in this subclass.

Acked-by: Frediano Ziglio <fziglio@redhat.com>

server/stream.c
@@ -758,6 +758,16 @@ void stream_agent_stop(StreamAgent *agent)
     }
 }
 
+static void upgrade_item_free(UpgradeItem *item)
+{
+    g_return_if_fail(item != NULL);
+    g_return_if_fail(item->base.refcount != 0);
+
+    drawable_unref(item->drawable);
+    free(item->rects);
+    free(item);
+}
+
 /*
  * after dcc_detach_stream_gracefully is called for all the display channel clients,
  * detach_stream should be called. See comment (1).
@@ -798,8 +808,8 @@ static void dcc_detach_stream_gracefully(DisplayChannelClient *dcc,
         rect_debug(&stream->current->red_drawable->bbox);
         rcc = RED_CHANNEL_CLIENT(dcc);
         upgrade_item = spice_new(UpgradeItem, 1);
-        upgrade_item->refs = 1;
-        pipe_item_init(&upgrade_item->base, PIPE_ITEM_TYPE_UPGRADE);
+        pipe_item_init_full(&upgrade_item->base, PIPE_ITEM_TYPE_UPGRADE,
+                            (GDestroyNotify)upgrade_item_free);
         upgrade_item->drawable = stream->current;
         upgrade_item->drawable->refs++;
         n_rects = pixman_region32_n_rects(&upgrade_item->drawable->tree_item.base.rgn);

server/dcc.c
@@ -1589,16 +1589,6 @@ int dcc_handle_migrate_data(DisplayChannelClient *dcc, uint32_t size, void *mess
     return TRUE;
 }
 
-static void upgrade_item_unref(UpgradeItem *item)
-{
-    if (--item->refs != 0)
-        return;
-
-    drawable_unref(item->drawable);
-    free(item->rects);
-    free(item);
-}
-
 static void release_item_after_push(PipeItem *item)
 {
     switch (item->type) {
@@ -1606,10 +1596,8 @@ static void release_item_after_push(PipeItem *item)
     case PIPE_ITEM_TYPE_IMAGE:
     case PIPE_ITEM_TYPE_STREAM_CLIP:
     case PIPE_ITEM_TYPE_MONITORS_CONFIG:
-        pipe_item_unref(item);
-        break;
     case PIPE_ITEM_TYPE_UPGRADE:
-        upgrade_item_unref((UpgradeItem *)item);
+        pipe_item_unref(item);
         break;
     case PIPE_ITEM_TYPE_GL_SCANOUT:
     case PIPE_ITEM_TYPE_GL_DRAW:
@@ -1647,8 +1635,6 @@ static void release_item_before_push(DisplayChannelClient *dcc, PipeItem *item)
     }
     case PIPE_ITEM_TYPE_STREAM_CLIP:
     case PIPE_ITEM_TYPE_UPGRADE:
-        upgrade_item_unref((UpgradeItem *)item);
-        break;
     case PIPE_ITEM_TYPE_IMAGE:
     case PIPE_ITEM_TYPE_MONITORS_CONFIG:
         pipe_item_unref(item);

server/display-channel.c
@@ -1971,10 +1971,8 @@ static void hold_item(RedChannelClient *rcc, PipeItem *item)
     case PIPE_ITEM_TYPE_DRAW:
     case PIPE_ITEM_TYPE_IMAGE:
     case PIPE_ITEM_TYPE_STREAM_CLIP:
-        pipe_item_ref(item);
-        break;
     case PIPE_ITEM_TYPE_UPGRADE:
-        ((UpgradeItem *)item)->refs++;
+        pipe_item_ref(item);
         break;
     default:
         spice_warn_if_reached();

server/display-channel.h
@@ -247,7 +247,6 @@ typedef struct SurfaceDestroyItem {
 
 typedef struct UpgradeItem {
     PipeItem base;
-    int refs;
     Drawable *drawable;
     SpiceClipRects *rects;
 } UpgradeItem;

commit 2c3fc80e5187405756fa2d29fad1696588f15c51

fix wrong assert causing streaming to crash
This regression was introduced by commit
18d3876b78c7f19c577da000d322dbd44e63e456
("UpgradeItem: use base PipeItem for refcounting")

This fix https://bugs.freedesktop.org/show_bug.cgi?id=95365.

Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
Acked-by: Christophe Fergeau <cfergeau@redhat.com>

server/stream.c
@@ -761,7 +761,7 @@ void stream_agent_stop(StreamAgent *agent)
 static void red_upgrade_item_free(RedUpgradeItem *item)
 {
     g_return_if_fail(item != NULL);
-    g_return_if_fail(item->base.refcount != 0);
+    g_return_if_fail(item->base.refcount == 0);
 
     drawable_unref(item->drawable);
     free(item->rects);