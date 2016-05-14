0c91530b07d2991293360ba72c391913049c7236
Frediano Ziglio <fziglio@redhat.com>
2016-05-14 14:00:29 +0100

    dc: join the two loop
    They tested the same condition, no break in inside loop (which would
    exit all loops)
    
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>

--- server/display-channel.c
+++ server/display-channel.c
@@ -479,16 +479,14 @@ static int current_add_equal(DisplayChannel *display, DrawItem *item, TreeItem *
             dpi_ring_item = ring_get_head(&other_drawable->pipes);
             /* dpi contains a sublist of dcc's, ordered the same */
             while (link) {
-                while (link) {
-                    dcc = link->data;
-                    dpi = SPICE_UPCAST(RedDrawablePipeItem, dpi_ring_item);
-                    if (dpi && dcc == dpi->dcc) {
-                        dpi_ring_item = ring_next(&other_drawable->pipes, dpi_ring_item);
-                    } else {
-                        dcc_prepend_drawable(dcc, drawable);
-                    }
-                    link = link->next;
+                dcc = link->data;
+                dpi = SPICE_UPCAST(RedDrawablePipeItem, dpi_ring_item);
+                if (dpi && dcc == dpi->dcc) {
+                    dpi_ring_item = ring_next(&other_drawable->pipes, dpi_ring_item);
+                } else {
+                    dcc_prepend_drawable(dcc, drawable);
                 }
+                link = link->next;
             }
             /* not sending other_drawable where possible */
             drawable_remove_from_pipes(other_drawable);
