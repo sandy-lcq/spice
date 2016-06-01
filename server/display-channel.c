sandy-lcq/spice

commit 4028fb1c794ef2a212f22b024b1c39f460439e7b

Replace RedChannel::clients with GList
Instead of using a Ring, use a GList to store the list of channel
clients. This allows us to iterate the clients without poking inside of
the client struct to get the channel_link. This is required in order to
make the RedChannelClient struct private.

server/display-channel.c
@@ -223,7 +223,7 @@ void display_channel_surface_unref(DisplayChannel *display, uint32_t surface_id)
     RedSurface *surface = &display->surfaces[surface_id];
     QXLInstance *qxl = display->common.qxl;
     DisplayChannelClient *dcc;
-    RingItem *link, *next;
+    GList *link, *next;
 
     if (--surface->refs != 0) {
         return;
@@ -256,7 +256,7 @@ static void streams_update_visible_region(DisplayChannel *display, Drawable *dra
 {
     Ring *ring;
     RingItem *item;
-    RingItem *dcc_ring_item, *next;
+    GList *link, *next;
     DisplayChannelClient *dcc;
 
     if (!red_channel_is_connected(RED_CHANNEL(display))) {
@@ -280,7 +280,7 @@ static void streams_update_visible_region(DisplayChannel *display, Drawable *dra
             continue;
         }
 
-        FOREACH_DCC(display, dcc_ring_item, next, dcc) {
+        FOREACH_DCC(display, link, next, dcc) {
             agent = &dcc->stream_agents[get_stream_id(display, stream)];
 
             if (region_intersects(&agent->vis_region, &drawable->tree_item.base.rgn)) {
@@ -295,10 +295,10 @@ static void streams_update_visible_region(DisplayChannel *display, Drawable *dra
 static void pipes_add_drawable(DisplayChannel *display, Drawable *drawable)
 {
     DisplayChannelClient *dcc;
-    RingItem *dcc_ring_item, *next;
+    GList *link, *next;
 
     spice_warn_if_fail(ring_is_empty(&drawable->pipes));
-    FOREACH_DCC(display, dcc_ring_item, next, dcc) {
+    FOREACH_DCC(display, link, next, dcc) {
         dcc_prepend_drawable(dcc, drawable);
     }
 }
@@ -320,9 +320,9 @@ static void pipes_add_drawable_after(DisplayChannel *display,
         return;
     }
     if (num_other_linked != display->common.base.clients_num) {
-        RingItem *item, *next;
+        GList *link, *next;
         spice_debug("TODO: not O(n^2)");
-        FOREACH_DCC(display, item, next, dcc) {
+        FOREACH_DCC(display, link, next, dcc) {
             int sent = 0;
             DRAWABLE_FOREACH_DPI_SAFE(pos_after, dpi_link, dpi_next, dpi_pos_after) {
                 if (dpi_pos_after->dcc == dcc) {
@@ -467,34 +467,32 @@ static int current_add_equal(DisplayChannel *display, DrawItem *item, TreeItem *
 
             DisplayChannelClient *dcc;
             RedDrawablePipeItem *dpi;
-            RingItem *worker_ring_item, *dpi_ring_item;
+            RingItem *dpi_ring_item;
+            GList *link;
 
             other_drawable->refs++;
             current_remove_drawable(display, other_drawable);
 
             /* sending the drawable to clients that already received
              * (or will receive) other_drawable */
-            worker_ring_item = ring_get_head(&RED_CHANNEL(display)->clients);
+            link = RED_CHANNEL(display)->clients;
             dpi_ring_item = ring_get_head(&other_drawable->pipes);
             /* dpi contains a sublist of dcc's, ordered the same */
-            while (worker_ring_item) {
-                dcc = SPICE_CONTAINEROF(worker_ring_item, DisplayChannelClient,
-                                        common.base.channel_link);
+            while (link) {
+                dcc = link->data;
                 dpi = SPICE_UPCAST(RedDrawablePipeItem, dpi_ring_item);
-                while (worker_ring_item && (!dpi || dcc != dpi->dcc)) {
+                while (link && (!dpi || dcc != dpi->dcc)) {
                     dcc_prepend_drawable(dcc, drawable);
-                    worker_ring_item = ring_next(&RED_CHANNEL(display)->clients,
-                                                 worker_ring_item);
-                    dcc = SPICE_CONTAINEROF(worker_ring_item, DisplayChannelClient,
-                                            common.base.channel_link);
+                    link = link->next;
+                    if (link)
+                        dcc = link->data;
                 }
 
                 if (dpi_ring_item) {
                     dpi_ring_item = ring_next(&other_drawable->pipes, dpi_ring_item);
                 }
-                if (worker_ring_item) {
-                    worker_ring_item = ring_next(&RED_CHANNEL(display)->clients,
-                                                 worker_ring_item);
+                if (link) {
+                    link = link->next;
                 }
             }
             /* not sending other_drawable where possible */
@@ -1171,9 +1169,9 @@ int display_channel_wait_for_migrate_data(DisplayChannel *display)
     }
 
     spice_debug(NULL);
-    spice_warn_if_fail(channel->clients_num == 1);
+    spice_warn_if_fail(g_list_length(channel->clients) == 1);
 
-    rcc = SPICE_CONTAINEROF(ring_get_head(&channel->clients), RedChannelClient, channel_link);
+    rcc = g_list_nth_data(channel->clients, 0);
 
     red_channel_client_ref(rcc);
     for (;;) {
@@ -1210,24 +1208,24 @@ void display_channel_flush_all_surfaces(DisplayChannel *display)
 
 void display_channel_free_glz_drawables_to_free(DisplayChannel *display)
 {
-    RingItem *link, *next;
+    GList *link, *next;
     DisplayChannelClient *dcc;
 
     spice_return_if_fail(display);
 
-    DCC_FOREACH_SAFE(link, next, dcc, RED_CHANNEL(display)) {
+    FOREACH_DCC(display, link, next, dcc) {
         dcc_free_glz_drawables_to_free(dcc);
     }
 }
 
 void display_channel_free_glz_drawables(DisplayChannel *display)
 {
-    RingItem *link, *next;
+    GList *link, *next;
     DisplayChannelClient *dcc;
 
     spice_return_if_fail(display);
 
-    DCC_FOREACH_SAFE(link, next, dcc, RED_CHANNEL(display)) {
+    FOREACH_DCC(display, link, next, dcc) {
         dcc_free_glz_drawables(dcc);
     }
 }
@@ -1270,11 +1268,11 @@ void display_channel_free_some(DisplayChannel *display)
 {
     int n = 0;
     DisplayChannelClient *dcc;
-    RingItem *item, *next;
+    GList *link, *next;
 
     spice_debug("#draw=%d, #glz_draw=%d", display->drawable_count,
                 display->glz_drawable_count);
-    FOREACH_DCC(display, item, next, dcc) {
+    FOREACH_DCC(display, link, next, dcc) {
         GlzSharedDictionary *glz_dict = dcc ? dcc->glz_dict : NULL;
 
         if (glz_dict) {
@@ -1289,7 +1287,7 @@ void display_channel_free_some(DisplayChannel *display)
         free_one_drawable(display, TRUE);
     }
 
-    FOREACH_DCC(display, item, next, dcc) {
+    FOREACH_DCC(display, link, next, dcc) {
         GlzSharedDictionary *glz_dict = dcc ? dcc->glz_dict : NULL;
 
         if (glz_dict) {
@@ -1761,10 +1759,10 @@ void display_channel_update(DisplayChannel *display,
 static void clear_surface_drawables_from_pipes(DisplayChannel *display, int surface_id,
                                                int wait_if_used)
 {
-    RingItem *item, *next;
+    GList *link, *next;
     DisplayChannelClient *dcc;
 
-    FOREACH_DCC(display, item, next, dcc) {
+    FOREACH_DCC(display, link, next, dcc) {
         if (!dcc_clear_surface_drawables_from_pipe(dcc, surface_id, wait_if_used)) {
             red_channel_client_disconnect(RED_CHANNEL_CLIENT(dcc));
         }
@@ -1828,9 +1826,9 @@ void display_channel_destroy_surfaces(DisplayChannel *display)
 static void send_create_surface(DisplayChannel *display, int surface_id, int image_ready)
 {
     DisplayChannelClient *dcc;
-    RingItem *item, *next;
+    GList *link, *next;
 
-    FOREACH_DCC(display, item, next, dcc) {
+    FOREACH_DCC(display, link, next, dcc) {
         dcc_create_surface(dcc, surface_id);
         if (image_ready)
             dcc_push_surface_image(dcc, surface_id);

server/main-channel.c
@@ -42,12 +42,12 @@ static void main_channel_client_on_disconnect(RedChannelClient *rcc)
 
 RedClient *main_channel_get_client_by_link_id(MainChannel *main_chan, uint32_t connection_id)
 {
-    RingItem *link;
+    GList *link;
     MainChannelClient *mcc;
     RedChannelClient *rcc;
 
-    RING_FOREACH(link, &main_chan->base.clients) {
-        rcc = SPICE_CONTAINEROF(link, RedChannelClient, channel_link);
+    for (link = main_chan->base.clients; link != NULL; link = link->next) {
+        rcc = link->data;
         mcc = (MainChannelClient*) rcc;
         if (main_channel_client_get_connection_id(mcc) == connection_id) {
             return rcc->client;
@@ -328,11 +328,10 @@ MainChannel* main_channel_new(RedsState *reds)
 
 static int main_channel_connect_semi_seamless(MainChannel *main_channel)
 {
-    RingItem *client_link;
+    GList *link;
 
-    RING_FOREACH(client_link, &main_channel->base.clients) {
-        RedChannelClient *rcc = SPICE_CONTAINEROF(client_link, RedChannelClient,
-                                                    channel_link);
+    for (link = main_channel->base.clients; link != NULL; link = link->next) {
+        RedChannelClient *rcc = link->data;
         MainChannelClient *mcc = (MainChannelClient*)rcc;
         if (main_channel_client_connect_semi_seamless(mcc))
             main_channel->num_clients_mig_wait++;
@@ -342,13 +341,12 @@ static int main_channel_connect_semi_seamless(MainChannel *main_channel)
 
 static int main_channel_connect_seamless(MainChannel *main_channel)
 {
-    RingItem *client_link;
+    GList *link;
 
-    spice_assert(main_channel->base.clients_num == 1);
+    spice_assert(g_list_length(main_channel->base.clients) == 1);
 
-    RING_FOREACH(client_link, &main_channel->base.clients) {
-        RedChannelClient *rcc = SPICE_CONTAINEROF(client_link, RedChannelClient,
-                                                    channel_link);
+    for (link = main_channel->base.clients; link != NULL; link = link->next) {
+        RedChannelClient *rcc = link->data;
         MainChannelClient *mcc = (MainChannelClient*)rcc;
         main_channel_client_connect_seamless(mcc);
         main_channel->num_clients_mig_wait++;
@@ -369,11 +367,9 @@ int main_channel_migrate_connect(MainChannel *main_channel, RedsMigSpice *mig_ta
     if (!try_seamless) {
         return main_channel_connect_semi_seamless(main_channel);
     } else {
-        RingItem *client_item;
         RedChannelClient *rcc;
 
-        client_item = ring_get_head(&main_channel->base.clients);
-        rcc = SPICE_CONTAINEROF(client_item, RedChannelClient, channel_link);
+        rcc = g_list_nth_data(main_channel->base.clients, 0);
 
         if (!red_channel_client_test_remote_cap(rcc,
                                                 SPICE_MAIN_CAP_SEAMLESS_MIGRATE)) {
@@ -387,11 +383,10 @@ int main_channel_migrate_connect(MainChannel *main_channel, RedsMigSpice *mig_ta
 
 void main_channel_migrate_cancel_wait(MainChannel *main_chan)
 {
-    RingItem *client_link;
+    GList *link;
 
-    RING_FOREACH(client_link, &main_chan->base.clients) {
-        RedChannelClient *rcc = SPICE_CONTAINEROF(client_link, RedChannelClient,
-                                                    channel_link);
+    for (link = main_chan->base.clients; link != NULL; link = link->next) {
+        RedChannelClient *rcc = link->data;
         MainChannelClient *mcc = (MainChannelClient*)rcc;
         main_channel_client_migrate_cancel_wait(mcc);
     }
@@ -400,19 +395,18 @@ void main_channel_migrate_cancel_wait(MainChannel *main_chan)
 
 int main_channel_migrate_src_complete(MainChannel *main_chan, int success)
 {
-    RingItem *client_link;
+    GList *link;
     int semi_seamless_count = 0;
 
     spice_printerr("");
 
-    if (ring_is_empty(&main_chan->base.clients)) {
+    if (!main_chan->base.clients) {
         spice_printerr("no peer connected");
         return 0;
     }
 
-    RING_FOREACH(client_link, &main_chan->base.clients) {
-        RedChannelClient *rcc = SPICE_CONTAINEROF(client_link, RedChannelClient,
-                                                    channel_link);
+    for (link = main_chan->base.clients; link != NULL; link = link->next) {
+        RedChannelClient *rcc = link->data;
         MainChannelClient *mcc = (MainChannelClient*)rcc;
         if (main_channel_client_migrate_src_complete(mcc, success))
             semi_seamless_count++;

server/red-channel.h
@@ -307,7 +307,7 @@ struct RedChannel {
     // However RCC still holds a reference to the Channel.
     // Maybe replace these logic with ref count?
     // TODO: rename to 'connected_clients'?
-    Ring clients;
+    GList *clients;
     uint32_t clients_num;
 
     OutgoingHandlerInterface outgoing_cb;
@@ -548,6 +548,7 @@ uint32_t red_channel_sum_pipes_size(RedChannel *channel);
 typedef void (*channel_client_callback)(RedChannelClient *rcc);
 typedef void (*channel_client_callback_data)(RedChannelClient *rcc, void *data);
 void red_channel_apply_clients(RedChannel *channel, channel_client_callback v);
+void red_channel_apply_clients_data(RedChannel *channel, channel_client_callback_data v, void * data);
 struct RedsState* red_channel_get_server(RedChannel *channel);
 
 struct RedClient {

server/red-worker.c
@@ -500,7 +500,7 @@ static void guest_set_client_capabilities(RedWorker *worker)
     int i;
     DisplayChannelClient *dcc;
     RedChannelClient *rcc;
-    RingItem *link, *next;
+    GList *link, *next;
     uint8_t caps[SPICE_CAPABILITIES_SIZE] = { 0 };
     int caps_available[] = {
         SPICE_DISPLAY_CAP_SIZED_STREAM,
@@ -533,7 +533,7 @@ static void guest_set_client_capabilities(RedWorker *worker)
         for (i = 0 ; i < sizeof(caps_available) / sizeof(caps_available[0]); ++i) {
             SET_CAP(caps, caps_available[i]);
         }
-        DCC_FOREACH_SAFE(link, next, dcc, RED_CHANNEL(worker->display_channel)) {
+        FOREACH_DCC(worker->display_channel, link, next, dcc) {
             rcc = (RedChannelClient *)dcc;
             for (i = 0 ; i < sizeof(caps_available) / sizeof(caps_available[0]); ++i) {
                 if (!red_channel_client_test_remote_cap(rcc, caps_available[i]))
@@ -646,7 +646,7 @@ static void display_update_monitors_config(DisplayChannel *display,
 static void red_worker_push_monitors_config(RedWorker *worker)
 {
     DisplayChannelClient *dcc;
-    RingItem *item, *next;
+    GList *item, *next;
 
     FOREACH_DCC(worker->display_channel, item, next, dcc) {
         dcc_push_monitors_config(dcc);

server/display-channel.h
@@ -226,14 +226,14 @@ struct DisplayChannel {
     stat_info_t lz4_stat;
 };
 
-#define LINK_TO_DCC(ptr) SPICE_CONTAINEROF(ptr, DisplayChannelClient,   \
-                                           common.base.channel_link)
-#define DCC_FOREACH_SAFE(link, next, dcc, channel)                      \
-    SAFE_FOREACH(link, next, channel,  &(channel)->clients, dcc, LINK_TO_DCC(link))
-
-
-#define FOREACH_DCC(display_channel, link, next, dcc)                   \
-    DCC_FOREACH_SAFE(link, next, dcc, RED_CHANNEL(display_channel))
+#define FOREACH_DCC(channel, _link, _next, _data)                   \
+    for (_link = (channel ? RED_CHANNEL(channel)->clients : NULL), \
+         _next = (_link ? _link->next : NULL), \
+         _data = (_link ? _link->data : NULL); \
+         _link; \
+         _link = _next, \
+         _next = (_link ? _link->next : NULL), \
+         _data = (_link ? _link->data : NULL))
 
 static inline int get_stream_id(DisplayChannel *display, Stream *stream)
 {

commit a1715a01d43621c15018a9d202f7683dc3cd1e19

channel: Remove clients_num and use g_list_length
This fixes a regression introduced by
4028fb1c794ef2a212f22b024b1c39f460439e7b, where clients_num
was not updated correctly anymore.

There is no reason to cache the number of elements on that list,
as the expected number of clients is quite small.

Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
Acked-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

server/display-channel.c
@@ -319,7 +319,7 @@ static void pipes_add_drawable_after(DisplayChannel *display,
         pipes_add_drawable(display, drawable);
         return;
     }
-    if (num_other_linked != display->common.base.clients_num) {
+    if (num_other_linked != g_list_length(display->common.base.clients)) {
         GList *link, *next;
         spice_debug("TODO: not O(n^2)");
         FOREACH_CLIENT(display, link, next, dcc) {

server/main-channel-client.c
@@ -457,7 +457,7 @@ void main_channel_client_migrate_dst_complete(MainChannelClient *mcc)
 {
     if (mcc->mig_wait_prev_complete) {
         if (mcc->mig_wait_prev_try_seamless) {
-            spice_assert(mcc->base.channel->clients_num == 1);
+            spice_assert(g_list_length(mcc->base.channel->clients) == 1);
             red_channel_client_pipe_add_type(&mcc->base,
                                              RED_PIPE_ITEM_TYPE_MAIN_MIGRATE_BEGIN_SEAMLESS);
         } else {

server/main-channel.c
@@ -102,7 +102,7 @@ static int main_channel_handle_migrate_data(RedChannelClient *rcc,
     SpiceMigrateDataHeader *header = (SpiceMigrateDataHeader *)message;
 
     /* not supported with multi-clients */
-    spice_assert(rcc->channel->clients_num == 1);
+    spice_assert(g_list_length(rcc->channel->clients) == 1);
 
     if (size < sizeof(SpiceMigrateDataHeader) + sizeof(SpiceMigrateDataMain)) {
         spice_printerr("bad message size %u", size);

server/red-channel.h
@@ -306,7 +306,6 @@ struct RedChannel {
     // Maybe replace these logic with ref count?
     // TODO: rename to 'connected_clients'?
     GList *clients;
-    uint32_t clients_num;
 
     OutgoingHandlerInterface outgoing_cb;
     IncomingHandlerInterface incoming_cb;

server/red-worker.c
@@ -525,7 +525,7 @@ static void guest_set_client_capabilities(RedWorker *worker)
         return;
     }
     if ((worker->display_channel == NULL) ||
-        (RED_CHANNEL(worker->display_channel)->clients_num == 0)) {
+        (RED_CHANNEL(worker->display_channel)->clients == NULL)) {
         red_qxl_set_client_capabilities(worker->qxl, FALSE, caps);
     } else {
         // Take least common denominator