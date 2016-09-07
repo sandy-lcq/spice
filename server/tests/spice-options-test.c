179616009596a4f540c6c7308ef465145035eecb
Pavel Grunt <pgrunt@redhat.com>
2016-09-07 09:26:23 +0200

    tests: Check setting of agent properties
    Acked-by: Frediano Ziglio <fziglio@redhat.com>

---
+++ server/tests/spice-options-test.c
@@ -0,0 +1,54 @@
+/* -*- Mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+   Copyright (C) 2016 Red Hat, Inc.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+#include <config.h>
+#include <glib.h>
+#include <spice.h>
+
+#include "basic_event_loop.h"
+
+/* GLIB_CHECK_VERSION(2, 40, 0) */
+#ifndef g_assert_nonnull
+#define g_assert_nonnull g_assert
+#endif
+
+int main(int argc, char *argv[])
+{
+    SpiceCoreInterface *core ;
+    SpiceServer *server = spice_server_new();
+
+    g_assert_nonnull(server);
+
+    core = basic_event_loop_init();
+    g_assert_nonnull(core);
+
+    /* test before init */
+    spice_server_set_agent_mouse(server, 0);
+    spice_server_set_agent_copypaste(server, 0);
+    spice_server_set_agent_file_xfer(server, 0);
+
+    g_assert_cmpint(spice_server_init(server, core), ==, 0);
+
+    /* test after init */
+    spice_server_set_agent_mouse(server, 0);
+    spice_server_set_agent_copypaste(server, 0);
+    spice_server_set_agent_file_xfer(server, 0);
+
+    spice_server_destroy(server);
+
+    return 0;
+}
