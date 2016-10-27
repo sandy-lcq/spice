sandy-lcq/spice

commit 96e94c6f32d4345ea25ef7a474fbd92de03b38ad

Convert RedChannel hierarchy to GObject
Acked-by: Frediano Ziglio <fziglio@redhat.com>
Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

server/dummy-channel.c
@@ -1,0 +1,49 @@
+/* dummy-channel.c */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "dummy-channel.h"
+
+G_DEFINE_TYPE(DummyChannel, dummy_channel, RED_TYPE_CHANNEL)
+
+static void
+dummy_channel_class_init(DummyChannelClass *klass)
+{
+}
+
+static void
+dummy_channel_init(DummyChannel *self)
+{
+}
+
+// TODO: red_worker can use this one
+static void dummy_watch_update_mask(SpiceWatch *watch, int event_mask)
+{
+}
+
+static SpiceWatch *dummy_watch_add(int fd, int event_mask, SpiceWatchFunc func, void *opaque)
+{
+    return NULL; // apparently allowed?
+}
+
+static void dummy_watch_remove(SpiceWatch *watch)
+{
+}
+
+// TODO: actually, since I also use channel_client_dummym, no need for core. Can be NULL
+static const SpiceCoreInterface dummy_core = {
+    .watch_update_mask = dummy_watch_update_mask,
+    .watch_add = dummy_watch_add,
+    .watch_remove = dummy_watch_remove,
+};
+
+RedChannel *dummy_channel_new(RedsState *reds, uint32_t type, uint32_t id)
+{
+    return g_object_new(TYPE_DUMMY_CHANNEL,
+                        "spice-server", reds,
+                        "core-interface", &dummy_core,
+                        "channel-type", type,
+                        "id", id,
+                        NULL);
+}

server/Makefile.am
@@ -102,6 +102,8 @@ libserver_la_SOURCES =				\
 	red-channel-client.c			\
 	red-channel-client.h			\
 	red-channel-client-private.h		\
+	dummy-channel.c				\
+	dummy-channel.h				\
 	dummy-channel-client.c			\
 	dummy-channel-client.h			\
 	red-common.h				\

server/common-graphics-channel.c
@@ -29,6 +29,11 @@
 
 #define CHANNEL_RECEIVE_BUF_SIZE 1024
 
+G_DEFINE_ABSTRACT_TYPE(CommonGraphicsChannel, common_graphics_channel, RED_TYPE_CHANNEL)
+
+#define GRAPHICS_CHANNEL_PRIVATE(o) \
+    (G_TYPE_INSTANCE_GET_PRIVATE((o), TYPE_COMMON_GRAPHICS_CHANNEL, CommonGraphicsChannelPrivate))
+
 struct CommonGraphicsChannelPrivate
 {
     QXLInstance *qxl;
@@ -43,7 +48,7 @@ struct CommonGraphicsChannelPrivate
 static uint8_t *common_alloc_recv_buf(RedChannelClient *rcc, uint16_t type, uint32_t size)
 {
     RedChannel *channel = red_channel_client_get_channel(rcc);
-    CommonGraphicsChannel *common = SPICE_CONTAINEROF(channel, CommonGraphicsChannel, base);
+    CommonGraphicsChannel *common = COMMON_GRAPHICS_CHANNEL(channel);
 
     /* SPICE_MSGC_MIGRATE_DATA is the only client message whose size is dynamic */
     if (type == SPICE_MSGC_MIGRATE_DATA) {
@@ -62,6 +67,48 @@ static void common_release_recv_buf(RedChannelClient *rcc, uint16_t type, uint32
 {
     if (type == SPICE_MSGC_MIGRATE_DATA) {
         free(msg);
+    }
+}
+
+
+enum {
+    PROP0,
+    PROP_QXL
+};
+
+static void
+common_graphics_channel_get_property(GObject *object,
+                                   guint property_id,
+                                   GValue *value,
+                                   GParamSpec *pspec)
+{
+    CommonGraphicsChannel *self = COMMON_GRAPHICS_CHANNEL(object);
+
+    switch (property_id)
+    {
+        case PROP_QXL:
+            g_value_set_pointer(value, self->priv->qxl);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+    }
+}
+
+static void
+common_graphics_channel_set_property(GObject *object,
+                                   guint property_id,
+                                   const GValue *value,
+                                   GParamSpec *pspec)
+{
+    CommonGraphicsChannel *self = COMMON_GRAPHICS_CHANNEL(object);
+
+    switch (property_id)
+    {
+        case PROP_QXL:
+            self->priv->qxl = g_value_get_pointer(value);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
     }
 }
 
@@ -106,40 +153,36 @@ int common_channel_config_socket(RedChannelClient *rcc)
     return TRUE;
 }
 
-CommonGraphicsChannel* common_graphics_channel_new(RedsState *server,
-                                                   QXLInstance *qxl,
-                                                   const SpiceCoreInterfaceInternal *core,
-                                                   int size, uint32_t channel_type,
-                                                   int migration_flags,
-                                                   ChannelCbs *channel_cbs,
-                                                   channel_handle_parsed_proc handle_parsed)
+
+static void
+common_graphics_channel_class_init(CommonGraphicsChannelClass *klass)
 {
-    RedChannel *channel = NULL;
-    CommonGraphicsChannel *common;
+    GObjectClass *object_class = G_OBJECT_CLASS(klass);
+    RedChannelClass *channel_class = RED_CHANNEL_CLASS(klass);
 
-    spice_return_val_if_fail(channel_cbs, NULL);
-    spice_return_val_if_fail(!channel_cbs->alloc_recv_buf, NULL);
-    spice_return_val_if_fail(!channel_cbs->release_recv_buf, NULL);
+    g_type_class_add_private(klass, sizeof(CommonGraphicsChannelPrivate));
 
-    if (!channel_cbs->config_socket)
-        channel_cbs->config_socket = common_channel_config_socket;
-    channel_cbs->alloc_recv_buf = common_alloc_recv_buf;
-    channel_cbs->release_recv_buf = common_release_recv_buf;
+    object_class->get_property = common_graphics_channel_get_property;
+    object_class->set_property = common_graphics_channel_set_property;
 
-    channel = red_channel_create_parser(size, server,
-                                        core, channel_type,
-                                        qxl->id, TRUE /* handle_acks */,
-                                        spice_get_client_channel_parser(channel_type, NULL),
-                                        handle_parsed,
-                                        channel_cbs,
-                                        migration_flags);
-    spice_return_val_if_fail(channel, NULL);
+    channel_class->config_socket = common_channel_config_socket;
+    channel_class->alloc_recv_buf = common_alloc_recv_buf;
+    channel_class->release_recv_buf = common_release_recv_buf;
 
-    common = COMMON_GRAPHICS_CHANNEL(channel);
-    /* FIXME remove leak */
-    common->priv = g_new0(CommonGraphicsChannelPrivate, 1);
-    common->priv->qxl = qxl;
-    return common;
+    g_object_class_install_property(object_class,
+                                    PROP_QXL,
+                                    g_param_spec_pointer("qxl",
+                                                         "qxl",
+                                                         "QXLInstance for this channel",
+                                                         G_PARAM_READWRITE |
+                                                         G_PARAM_CONSTRUCT_ONLY |
+                                                         G_PARAM_STATIC_STRINGS));
+}
+
+static void
+common_graphics_channel_init(CommonGraphicsChannel *self)
+{
+    self->priv = GRAPHICS_CHANNEL_PRIVATE(self);
 }
 
 void common_graphics_channel_set_during_target_migrate(CommonGraphicsChannel *self, gboolean value)

server/common-graphics-channel.h
@@ -18,21 +18,47 @@
 #ifndef _COMMON_GRAPHICS_CHANNEL_H
 #define _COMMON_GRAPHICS_CHANNEL_H
 
+#include <glib-object.h>
+
 #include "red-channel.h"
 #include "red-channel-client.h"
+
+G_BEGIN_DECLS
 
 int common_channel_config_socket(RedChannelClient *rcc);
 
 #define COMMON_CLIENT_TIMEOUT (NSEC_PER_SEC * 30)
 
+#define TYPE_COMMON_GRAPHICS_CHANNEL common_graphics_channel_get_type()
+
+#define COMMON_GRAPHICS_CHANNEL(obj) \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), TYPE_COMMON_GRAPHICS_CHANNEL, CommonGraphicsChannel))
+#define COMMON_GRAPHICS_CHANNEL_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_CAST((klass), TYPE_COMMON_GRAPHICS_CHANNEL, CommonGraphicsChannelClass))
+#define COMMON_IS_GRAPHICS_CHANNEL(obj) \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj), TYPE_COMMON_GRAPHICS_CHANNEL))
+#define COMMON_IS_GRAPHICS_CHANNEL_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_TYPE((klass), TYPE_COMMON_GRAPHICS_CHANNEL))
+#define COMMON_GRAPHICS_CHANNEL_GET_CLASS(obj) \
+    (G_TYPE_INSTANCE_GET_CLASS((obj), TYPE_COMMON_GRAPHICS_CHANNEL, CommonGraphicsChannelClass))
+
+typedef struct CommonGraphicsChannel CommonGraphicsChannel;
+typedef struct CommonGraphicsChannelClass CommonGraphicsChannelClass;
 typedef struct CommonGraphicsChannelPrivate CommonGraphicsChannelPrivate;
-typedef struct CommonGraphicsChannel {
-    RedChannel base; // Must be the first thing
+
+struct CommonGraphicsChannel
+{
+    RedChannel parent;
 
     CommonGraphicsChannelPrivate *priv;
-} CommonGraphicsChannel;
+};
 
-#define COMMON_GRAPHICS_CHANNEL(Channel) ((CommonGraphicsChannel*)(Channel))
+struct CommonGraphicsChannelClass
+{
+    RedChannelClass parent_class;
+};
+
+GType common_graphics_channel_get_type(void) G_GNUC_CONST;
 
 void common_graphics_channel_set_during_target_migrate(CommonGraphicsChannel *self, gboolean value);
 gboolean common_graphics_channel_get_during_target_migrate(CommonGraphicsChannel *self);
@@ -76,12 +102,6 @@ static inline void red_pipes_add_verb(RedChannel *channel, uint16_t verb)
     red_channel_apply_clients_data(channel, red_pipe_add_verb_proxy, GUINT_TO_POINTER(verb));
 }
 
-CommonGraphicsChannel* common_graphics_channel_new(RedsState *server,
-                                                   QXLInstance *qxl,
-                                                   const SpiceCoreInterfaceInternal *core,
-                                                   int size, uint32_t channel_type,
-                                                   int migration_flags,
-                                                   ChannelCbs *channel_cbs,
-                                                   channel_handle_parsed_proc handle_parsed);
+G_END_DECLS
 
 #endif /* _COMMON_GRAPHICS_CHANNEL_H */

server/cursor-channel.c
@@ -28,8 +28,6 @@
 #include "reds.h"
 #include "red-qxl.h"
 
-#define CURSOR_CHANNEL(channel) ((CursorChannel*)(channel))
-
 typedef struct CursorItem {
     QXLInstance *qxl;
     int refs;
@@ -38,13 +36,10 @@ typedef struct CursorItem {
 
 G_STATIC_ASSERT(sizeof(CursorItem) <= QXL_CURSUR_DEVICE_DATA_SIZE);
 
-typedef struct RedCursorPipeItem {
-    RedPipeItem base;
-    CursorItem *cursor_item;
-} RedCursorPipeItem;
+struct CursorChannel
+{
+    CommonGraphicsChannel parent;
 
-typedef struct CursorChannelPrivate CursorChannelPrivate;
-struct CursorChannelPrivate {
     CursorItem *item;
     int cursor_visible;
     SpicePoint16 cursor_position;
@@ -53,11 +48,17 @@ struct CursorChannelPrivate {
     uint32_t mouse_mode;
 };
 
-struct CursorChannel {
-    CommonGraphicsChannel common; // Must be the first thing
-
-    CursorChannelPrivate priv[1];
+struct CursorChannelClass
+{
+    CommonGraphicsChannelClass parent_class;
 };
+
+typedef struct RedCursorPipeItem {
+    RedPipeItem base;
+    CursorItem *cursor_item;
+} RedCursorPipeItem;
+
+G_DEFINE_TYPE(CursorChannel, cursor_channel, TYPE_COMMON_GRAPHICS_CHANNEL)
 
 static void cursor_pipe_item_free(RedPipeItem *pipe_item);
 
@@ -105,10 +106,10 @@ static void cursor_item_unref(CursorItem *item)
 
 static void cursor_set_item(CursorChannel *cursor, CursorItem *item)
 {
-    if (cursor->priv->item)
-        cursor_item_unref(cursor->priv->item);
+    if (cursor->item)
+        cursor_item_unref(cursor->item);
 
-    cursor->priv->item = item ? cursor_item_ref(item) : NULL;
+    cursor->item = item ? cursor_item_ref(item) : NULL;
 }
 
 static RedPipeItem *new_cursor_pipe_item(RedChannelClient *rcc, void *data, int num)
@@ -197,12 +198,12 @@ static void red_marshall_cursor_init(CursorChannelClient *ccc, SpiceMarshaller *
     cursor_channel = CURSOR_CHANNEL(red_channel_client_get_channel(rcc));
 
     red_channel_client_init_send_data(rcc, SPICE_MSG_CURSOR_INIT, NULL);
-    msg.visible = cursor_channel->priv->cursor_visible;
-    msg.position = cursor_channel->priv->cursor_position;
-    msg.trail_length = cursor_channel->priv->cursor_trail_length;
-    msg.trail_frequency = cursor_channel->priv->cursor_trail_frequency;
+    msg.visible = cursor_channel->cursor_visible;
+    msg.position = cursor_channel->cursor_position;
+    msg.trail_length = cursor_channel->cursor_trail_length;
+    msg.trail_frequency = cursor_channel->cursor_trail_frequency;
 
-    cursor_fill(ccc, &msg.cursor, cursor_channel->priv->item, &info);
+    cursor_fill(ccc, &msg.cursor, cursor_channel->item, &info);
     spice_marshall_msg_cursor_init(base_marshaller, &msg);
     add_buf_from_info(base_marshaller, &info);
 }
@@ -212,8 +213,7 @@ static void cursor_marshall(CursorChannelClient *ccc,
                             RedCursorPipeItem *cursor_pipe_item)
 {
     RedChannelClient *rcc = RED_CHANNEL_CLIENT(ccc);
-    CursorChannel *cursor_channel = SPICE_CONTAINEROF(red_channel_client_get_channel(rcc),
-                                                      CursorChannel, common.base);
+    CursorChannel *cursor_channel = CURSOR_CHANNEL(red_channel_client_get_channel(rcc));
     CursorItem *item = cursor_pipe_item->cursor_item;
     RedPipeItem *pipe_item = &cursor_pipe_item->base;
     RedCursorCmd *cmd;
@@ -237,7 +237,7 @@ static void cursor_marshall(CursorChannelClient *ccc,
 
             red_channel_client_init_send_data(rcc, SPICE_MSG_CURSOR_SET, pipe_item);
             cursor_set.position = cmd->u.set.position;
-            cursor_set.visible = cursor_channel->priv->cursor_visible;
+            cursor_set.visible = cursor_channel->cursor_visible;
 
             cursor_fill(ccc, &cursor_set.cursor, item, &info);
             spice_marshall_msg_cursor_set(m, &cursor_set);
@@ -307,24 +307,14 @@ static void cursor_channel_send_item(RedChannelClient *rcc, RedPipeItem *pipe_it
 CursorChannel* cursor_channel_new(RedsState *server, QXLInstance *qxl,
                                   const SpiceCoreInterfaceInternal *core)
 {
-    CursorChannel *cursor_channel;
-    CommonGraphicsChannel *channel = NULL;
-    ChannelCbs cbs = {
-        .on_disconnect =  cursor_channel_client_on_disconnect,
-        .send_item = cursor_channel_send_item,
-    };
-
     spice_info("create cursor channel");
-    channel = common_graphics_channel_new(server, qxl, core,
-                                          sizeof(CursorChannel),
-                                          SPICE_CHANNEL_CURSOR, 0,
-                                          &cbs, red_channel_client_handle_message);
-
-    cursor_channel = CURSOR_CHANNEL(channel);
-    cursor_channel->priv->cursor_visible = TRUE;
-    cursor_channel->priv->mouse_mode = SPICE_MOUSE_MODE_SERVER;
-
-    return cursor_channel;
+    return g_object_new(TYPE_CURSOR_CHANNEL,
+                        "spice-server", server,
+                        "core-interface", core,
+                        "channel-type", SPICE_CHANNEL_CURSOR,
+                        "migration-flags", 0,
+                        "qxl", qxl,
+                        NULL);
 }
 
 void cursor_channel_process_cmd(CursorChannel *cursor, RedCursorCmd *cursor_cmd)
@@ -341,31 +331,31 @@ void cursor_channel_process_cmd(CursorChannel *cursor, RedCursorCmd *cursor_cmd)
 
     switch (cursor_cmd->type) {
     case QXL_CURSOR_SET:
-        cursor->priv->cursor_visible = cursor_cmd->u.set.visible;
+        cursor->cursor_visible = cursor_cmd->u.set.visible;
         cursor_set_item(cursor, cursor_item);
         break;
     case QXL_CURSOR_MOVE:
-        cursor_show = !cursor->priv->cursor_visible;
-        cursor->priv->cursor_visible = TRUE;
-        cursor->priv->cursor_position = cursor_cmd->u.position;
+        cursor_show = !cursor->cursor_visible;
+        cursor->cursor_visible = TRUE;
+        cursor->cursor_position = cursor_cmd->u.position;
         break;
     case QXL_CURSOR_HIDE:
-        cursor->priv->cursor_visible = FALSE;
+        cursor->cursor_visible = FALSE;
         break;
     case QXL_CURSOR_TRAIL:
-        cursor->priv->cursor_trail_length = cursor_cmd->u.trail.length;
-        cursor->priv->cursor_trail_frequency = cursor_cmd->u.trail.frequency;
+        cursor->cursor_trail_length = cursor_cmd->u.trail.length;
+        cursor->cursor_trail_frequency = cursor_cmd->u.trail.frequency;
         break;
     default:
         spice_warning("invalid cursor command %u", cursor_cmd->type);
         return;
     }
 
-    if (red_channel_is_connected(&cursor->common.base) &&
-        (cursor->priv->mouse_mode == SPICE_MOUSE_MODE_SERVER
+    if (red_channel_is_connected(RED_CHANNEL(cursor)) &&
+        (cursor->mouse_mode == SPICE_MOUSE_MODE_SERVER
          || cursor_cmd->type != QXL_CURSOR_MOVE
          || cursor_show)) {
-        red_channel_pipes_new_add(&cursor->common.base,
+        red_channel_pipes_new_add(RED_CHANNEL(cursor),
                                   new_cursor_pipe_item, cursor_item);
     }
 
@@ -374,21 +364,21 @@ void cursor_channel_process_cmd(CursorChannel *cursor, RedCursorCmd *cursor_cmd)
 
 void cursor_channel_reset(CursorChannel *cursor)
 {
-    RedChannel *channel = &cursor->common.base;
+    RedChannel *channel = RED_CHANNEL(cursor);
 
     spice_return_if_fail(cursor);
 
     cursor_set_item(cursor, NULL);
-    cursor->priv->cursor_visible = TRUE;
-    cursor->priv->cursor_position.x = cursor->priv->cursor_position.y = 0;
-    cursor->priv->cursor_trail_length = cursor->priv->cursor_trail_frequency = 0;
+    cursor->cursor_visible = TRUE;
+    cursor->cursor_position.x = cursor->cursor_position.y = 0;
+    cursor->cursor_trail_length = cursor->cursor_trail_frequency = 0;
 
     if (red_channel_is_connected(channel)) {
         red_channel_pipes_add_type(channel, RED_PIPE_ITEM_TYPE_INVAL_CURSOR_CACHE);
         if (!common_graphics_channel_get_during_target_migrate(COMMON_GRAPHICS_CHANNEL(cursor))) {
             red_pipes_add_verb(channel, SPICE_MSG_CURSOR_RESET);
         }
-        if (!red_channel_wait_all_sent(&cursor->common.base,
+        if (!red_channel_wait_all_sent(channel,
                                        COMMON_CLIENT_TIMEOUT)) {
             red_channel_apply_clients(channel,
                                       red_channel_client_disconnect_if_pending_send);
@@ -400,7 +390,7 @@ static void cursor_channel_init_client(CursorChannel *cursor, CursorChannelClien
 {
     spice_return_if_fail(cursor);
 
-    if (!red_channel_is_connected(&cursor->common.base)
+    if (!red_channel_is_connected(RED_CHANNEL(cursor))
         || common_graphics_channel_get_during_target_migrate(COMMON_GRAPHICS_CHANNEL(cursor))) {
         spice_debug("during_target_migrate: skip init");
         return;
@@ -413,7 +403,7 @@ static void cursor_channel_init_client(CursorChannel *cursor, CursorChannelClien
         red_channel_pipes_add_type(RED_CHANNEL(cursor), RED_PIPE_ITEM_TYPE_CURSOR_INIT);
 }
 
-void cursor_channel_init(CursorChannel *cursor)
+void cursor_channel_do_init(CursorChannel *cursor)
 {
     cursor_channel_init_client(cursor, NULL);
 }
@@ -422,7 +412,7 @@ void cursor_channel_set_mouse_mode(CursorChannel *cursor, uint32_t mode)
 {
     spice_return_if_fail(cursor);
 
-    cursor->priv->mouse_mode = mode;
+    cursor->mouse_mode = mode;
 }
 
 void cursor_channel_connect(CursorChannel *cursor, RedClient *client, RedsStream *stream,
@@ -446,4 +436,23 @@ void cursor_channel_connect(CursorChannel *cursor, RedClient *client, RedsStream
     red_channel_client_push_set_ack(rcc);
 
     cursor_channel_init_client(cursor, ccc);
+}
+
+static void
+cursor_channel_class_init(CursorChannelClass *klass)
+{
+    RedChannelClass *channel_class = RED_CHANNEL_CLASS(klass);
+
+    channel_class->parser = spice_get_client_channel_parser(SPICE_CHANNEL_CURSOR, NULL);
+    channel_class->handle_parsed = red_channel_client_handle_message;
+
+    channel_class->on_disconnect =  cursor_channel_client_on_disconnect;
+    channel_class->send_item = cursor_channel_send_item;
+}
+
+static void
+cursor_channel_init(CursorChannel *self)
+{
+    self->cursor_visible = TRUE;
+    self->mouse_mode = SPICE_MOUSE_MODE_SERVER;
 }

server/cursor-channel.h
@@ -21,12 +21,27 @@
 #include "common-graphics-channel.h"
 #include "red-parse-qxl.h"
 
+G_BEGIN_DECLS
+
 /**
  * This type it's a RedChannel class which implement cursor (mouse)
  * movements.
  * A pointer to CursorChannel can be converted to a RedChannel.
  */
 typedef struct CursorChannel CursorChannel;
+typedef struct CursorChannelClass CursorChannelClass;
+
+#define TYPE_CURSOR_CHANNEL cursor_channel_get_type()
+
+#define CURSOR_CHANNEL(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), TYPE_CURSOR_CHANNEL, CursorChannel))
+#define CURSOR_CHANNEL_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_CAST((klass), TYPE_CURSOR_CHANNEL, CursorChannelClass))
+#define IS_CURSOR_CHANNEL(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), TYPE_CURSOR_CHANNEL))
+#define IS_CURSOR_CHANNEL_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), TYPE_CURSOR_CHANNEL))
+#define CURSOR_CHANNEL_GET_CLASS(obj) \
+    (G_TYPE_INSTANCE_GET_CLASS((obj), TYPE_CURSOR_CHANNEL, CursorChannelClass))
+
+GType cursor_channel_get_type(void) G_GNUC_CONST;
 
 /**
  * Create CursorChannel.
@@ -48,7 +63,7 @@ CursorChannel*       cursor_channel_new         (RedsState *server, QXLInstance 
  */
 void                 cursor_channel_disconnect  (CursorChannel *cursor);
 void                 cursor_channel_reset       (CursorChannel *cursor);
-void                 cursor_channel_init        (CursorChannel *cursor);
+void                 cursor_channel_do_init     (CursorChannel *cursor);
 void                 cursor_channel_process_cmd (CursorChannel *cursor, RedCursorCmd *cursor_cmd);
 void                 cursor_channel_set_mouse_mode(CursorChannel *cursor, uint32_t mode);
 
@@ -69,5 +84,7 @@ void                 cursor_channel_connect     (CursorChannel *cursor, RedClien
  * See comment on cursor_channel_new.
  */
 void                 cursor_channel_client_migrate(RedChannelClient *client);
+
+G_END_DECLS
 
 #endif /* CURSOR_CHANNEL_H_ */

server/dcc-send.c
@@ -20,7 +20,7 @@
 #endif
 
 #include "dcc-private.h"
-#include "display-channel.h"
+#include "display-channel-private.h"
 #include "red-channel-client-private.h"
 
 #include <common/marshaller.h>
@@ -185,8 +185,9 @@ static void red_display_add_image_to_pixmap_cache(RedChannelClient *rcc,
                                                   SpiceImage *image, SpiceImage *io_image,
                                                   int is_lossy)
 {
+    DisplayChannel *display_channel =
+        DISPLAY_CHANNEL(red_channel_client_get_channel(rcc));
     DisplayChannelClient *dcc = DISPLAY_CHANNEL_CLIENT(rcc);
-    DisplayChannel *display_channel = DCC_TO_DC(dcc);
 
     if ((image->descriptor.flags & SPICE_IMAGE_FLAGS_CACHE_ME)) {
         spice_assert(image->descriptor.width * image->descriptor.height > 0);
@@ -1817,7 +1818,7 @@ static void display_channel_marshall_migrate_data(RedChannelClient *rcc,
     ImageEncoders *encoders = dcc_get_encoders(dcc);
     SpiceMigrateDataDisplay display_data = {0,};
 
-    display_channel = DCC_TO_DC(dcc);
+    display_channel = DISPLAY_CHANNEL(red_channel_client_get_channel(rcc));
 
     red_channel_client_init_send_data(rcc, SPICE_MSG_MIGRATE_DATA, NULL);
     spice_marshaller_add_uint32(base_marshaller, SPICE_MIGRATE_DATA_DISPLAY_MAGIC);
@@ -2120,8 +2121,8 @@ static void marshall_qxl_drawable(RedChannelClient *rcc,
     spice_return_if_fail(rcc);
 
     Drawable *item = dpi->drawable;
-    DisplayChannel *display = SPICE_CONTAINEROF(red_channel_client_get_channel(rcc),
-                                                DisplayChannel, common.base);
+    DisplayChannel *display =
+        DISPLAY_CHANNEL(red_channel_client_get_channel(rcc));
 
     spice_return_if_fail(display);
     /* allow sized frames to be streamed, even if they where replaced by another frame, since

server/dcc.c
@@ -21,6 +21,7 @@
 
 #include "dcc-private.h"
 #include "display-channel.h"
+#include "display-channel-private.h"
 #include "red-channel-client-private.h"
 #include "main-channel-client.h"
 #include "spice-server-enums.h"

commit 13e9cf019467baf954dd0442da6d646d52957595

Fix core-interface type regression
This was introduced with 96e94c6f32d4345ea25ef7a474fbd92de03b38ad
(Convert RedChannel hierarchy to GObject).
The type for "core-interface" property should be
SpiceCoreInterfaceInternal, not SpiceCoreInterface.

Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
Acked-by: Christophe Fergeau <cfergeau@redhat.com>

server/dummy-channel.c
@@ -50,7 +50,8 @@ static void dummy_watch_update_mask(SpiceWatch *watch, int event_mask)
 {
 }
 
-static SpiceWatch *dummy_watch_add(int fd, int event_mask, SpiceWatchFunc func, void *opaque)
+static SpiceWatch *dummy_watch_add(const SpiceCoreInterfaceInternal *iface,
+                                   int fd, int event_mask, SpiceWatchFunc func, void *opaque)
 {
     return NULL; // apparently allowed?
 }
@@ -59,8 +60,8 @@ static void dummy_watch_remove(SpiceWatch *watch)
 {
 }
 
-// TODO: actually, since I also use channel_client_dummym, no need for core. Can be NULL
-static const SpiceCoreInterface dummy_core = {
+// TODO: actually, since I also use channel_client_dummy, no need for core. Can be NULL
+static const SpiceCoreInterfaceInternal dummy_core = {
     .watch_update_mask = dummy_watch_update_mask,
     .watch_add = dummy_watch_add,
     .watch_remove = dummy_watch_remove,