sandy-lcq/spice

commit 590acf3c5562e61d357e455e1f38be371da87557

Sound: change on_new_*_channel() signatures
Pass the new sound channel to these functions rather than setting
worker->connection before calling the function and expecting it to be
set when the function is called. The fewer unenforced assumptions that
must be true for a function to work properly, the easier it will be to
maintain.

Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
Acked-by: Frediano Ziglio <fziglio@redhat.com>

server/sound.c
@@ -1166,13 +1166,13 @@ static int snd_desired_audio_mode(int playback_compression, int frequency,
     return SPICE_AUDIO_DATA_MODE_RAW;
 }
 
-static void on_new_playback_channel(SndWorker *worker)
+static void on_new_playback_channel(SndWorker *worker, SndChannel *snd_channel)
 {
     RedsState *reds = red_channel_get_server(worker->base_channel);
-    SndChannel *snd_channel = worker->connection;
 
     spice_assert(snd_channel);
 
+    worker->connection = snd_channel;
     snd_set_command(snd_channel, SND_PLAYBACK_MODE_MASK);
     if (snd_channel->active) {
         snd_set_command(snd_channel, SND_PLAYBACK_CTRL_MASK);
@@ -1220,7 +1220,6 @@ static void snd_set_playback_peer(RedChannel *channel, RedClient *client, RedsSt
                                                               caps, num_caps))) {
         return;
     }
-    worker->connection = &playback_channel->base;
     snd_playback_free_frame(playback_channel, &playback_channel->frames[0]);
     snd_playback_free_frame(playback_channel, &playback_channel->frames[1]);
     snd_playback_free_frame(playback_channel, &playback_channel->frames[2]);
@@ -1243,7 +1242,7 @@ static void snd_set_playback_peer(RedChannel *channel, RedClient *client, RedsSt
     }
 
     if (!red_client_during_migrate_at_target(client)) {
-        on_new_playback_channel(worker);
+        on_new_playback_channel(worker, &playback_channel->base);
     }
 
     if (worker->active) {
@@ -1425,12 +1424,11 @@ SPICE_GNUC_VISIBLE void spice_server_set_record_rate(SpiceRecordInstance *sin, u
         red_channel_set_cap(channel, SPICE_RECORD_CAP_OPUS);
 }
 
-static void on_new_record_channel(SndWorker *worker)
+static void on_new_record_channel(SndWorker *worker, SndChannel *snd_channel)
 {
-    SndChannel *snd_channel = worker->connection;
-
     spice_assert(snd_channel);
 
+    worker->connection = snd_channel ;
     if (worker->volume.volume_nchannels) {
         snd_set_command(snd_channel, SND_RECORD_VOLUME_MASK);
     }
@@ -1472,9 +1470,7 @@ static void snd_set_record_peer(RedChannel *channel, RedClient *client, RedsStre
 
     record_channel->mode = SPICE_AUDIO_DATA_MODE_RAW;
 
-    worker->connection = &record_channel->base;
-
-    on_new_record_channel(worker);
+    on_new_record_channel(worker, &record_channel->base);
     if (worker->active) {
         spice_server_record_start(st->sin);
     }

commit 258c2234babed9d77d44c9e70753985f4f442b0b

playback: Don't lose client connection after migration
Commit 590acf3c556 introduced a regression after migration:
PlaybackChannel::connection is never set even if a client is connected,
which means the playback channel is non-functional until a client
reconnects as all the snd_channel_set_xxx() method checks for a non-NULL
PlaybackChannel::connection before sending data to the client.

This commit slightly changes the code flow in
on_new_playback_channel_client()/playback_channel_client_constructed()
so that PlaybackChannel::connection is set regardless of what
red_client_during_migrate_at_target() returns. This is what was done
prior to 590acf3c556.

This resolves https://bugs.freedesktop.org/show_bug.cgi?id=100136

Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>
Acked-by: Jonathon Jongsma <jjongsma@redhat.com>

server/sound.c
@@ -1010,10 +1010,14 @@ static int snd_desired_audio_mode(bool playback_compression, int frequency,
 static void on_new_playback_channel_client(SndChannel *channel, SndChannelClient *client)
 {
     RedsState *reds = red_channel_get_server(RED_CHANNEL(channel));
+    RedClient *red_client = red_channel_client_get_client(RED_CHANNEL_CLIENT(client));
 
     spice_assert(client);
 
     channel->connection = client;
+    if (red_client_during_migrate_at_target(red_client)) {
+        return;
+    }
     snd_set_command(client, SND_PLAYBACK_MODE_MASK);
     if (client->active) {
         snd_set_command(client, SND_CTRL_MASK);
@@ -1055,7 +1059,6 @@ playback_channel_client_constructed(GObject *object)
 {
     PlaybackChannelClient *playback_client = PLAYBACK_CHANNEL_CLIENT(object);
     RedChannel *red_channel = red_channel_client_get_channel(RED_CHANNEL_CLIENT(playback_client));
-    RedClient *client = red_channel_client_get_client(RED_CHANNEL_CLIENT(playback_client));
     SndChannel *channel = SND_CHANNEL(red_channel);
 
     G_OBJECT_CLASS(playback_channel_client_parent_class)->constructed(object);
@@ -1083,9 +1086,7 @@ playback_channel_client_constructed(GObject *object)
     spice_debug("playback client %p using mode %s", playback_client,
                 spice_audio_data_mode_to_string(playback_client->mode));
 
-    if (!red_client_during_migrate_at_target(client)) {
-        on_new_playback_channel_client(channel, SND_CHANNEL_CLIENT(playback_client));
-    }
+    on_new_playback_channel_client(channel, SND_CHANNEL_CLIENT(playback_client));
 
     if (channel->active) {
         snd_playback_start(channel);