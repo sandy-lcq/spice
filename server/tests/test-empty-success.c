8f872043b3c6623a3da005748035ac854028981c
Frediano Ziglio <fziglio@redhat.com>
2017-09-03 08:43:22 +0100

    tests: Make test-empty-success automated
    Add some check that something happened during creation/destruction.
    Set as running on "make check".
    
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>
    Acked-by: Christophe Fergeau <cfergeau@redhat.com>

--- server/tests/test-empty-success.c
+++ server/tests/test-empty-success.c
@@ -18,36 +18,40 @@
 #include <config.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <common/log.h>
 #include <spice.h>
 
-struct SpiceTimer {
-    int a,b;
-};
+static unsigned int watches_created = 0;
+static unsigned int timers_created = 0;
+
+static SpiceWatch *const dummy_watch = (SpiceWatch*)(uintptr_t)0xdeadbeef;
+static SpiceTimer *const dummy_timer = (SpiceTimer*)(uintptr_t)0xbeefdead;
 
 static SpiceTimer*
 timer_add(SPICE_GNUC_UNUSED SpiceTimerFunc func,
           SPICE_GNUC_UNUSED void *opaque)
 {
-    static struct SpiceTimer t = {0,};
-
-    return &t;
+    ++timers_created;
+    return dummy_timer;
 }
 
 static void
 timer_start(SPICE_GNUC_UNUSED SpiceTimer *timer,
             SPICE_GNUC_UNUSED uint32_t ms)
 {
+    spice_assert(timer == dummy_timer);
 }
 
 static void
 timer_cancel(SPICE_GNUC_UNUSED SpiceTimer *timer)
 {
+    spice_assert(timer == dummy_timer);
 }
 
 static void
 timer_remove(SPICE_GNUC_UNUSED SpiceTimer *timer)
 {
+    spice_assert(timer == dummy_timer);
 }
 
 static SpiceWatch *
@@ -56,18 +60,21 @@ watch_add(SPICE_GNUC_UNUSED int fd,
           SPICE_GNUC_UNUSED SpiceWatchFunc func,
           SPICE_GNUC_UNUSED void *opaque)
 {
-    return NULL;
+    ++watches_created;
+    return dummy_watch;
 }
 
 static void
 watch_update_mask(SPICE_GNUC_UNUSED SpiceWatch *watch,
                   SPICE_GNUC_UNUSED int event_mask)
 {
+    spice_assert(watch == dummy_watch);
 }
 
 static void
 watch_remove(SPICE_GNUC_UNUSED SpiceWatch *watch)
 {
+    spice_assert(watch == dummy_watch);
 }
 
 static void
@@ -96,6 +103,10 @@ int main(void)
     spice_server_init(server, &core);
 
     spice_server_destroy(server);
+
+    // should have created some timers and watch
+    spice_assert(watches_created > 0);
+    spice_assert(timers_created > 0);
 
     return 0;
 }
